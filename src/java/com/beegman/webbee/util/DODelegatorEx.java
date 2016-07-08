1b7e382a877a6af973b980ae5e65513fe92410b8
dmitriy <dmitriy@home>
2016-07-07 17:59:07 -0700

    made logo clickable  and added a convenient method

--- src/java/com/beegman/webbee/util/DODelegatorEx.java
+++ src/java/com/beegman/webbee/util/DODelegatorEx.java
@@ -1,17 +1,19 @@
 package com.beegman.webbee.util;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 import org.aldan3.annot.DBField;
 import org.aldan3.annot.FormField;
 import org.aldan3.data.DODelegator;
+import org.aldan3.model.Log;
 
 public class DODelegatorEx<T> extends DODelegator<T> {
 
 	public DODelegatorEx(T model, String exclude) {
 		super(model);
-		fieldsMap = new HashMap<String, java.lang.reflect.Field>();
+		fieldsMap.clear();
 		for (java.lang.reflect.Field f : principal.getClass().getFields()) {
 			FormField ff = f.getAnnotation(FormField.class);
 			if (f.getAnnotation(DBField.class) != null || ff != null) {
@@ -28,6 +30,43 @@ public class DODelegatorEx<T> extends DODelegator<T> {
 			while (st.hasMoreTokens())
 				fieldsMap.remove(normilizeFieldName(st.nextToken()));
 		}
+	}
+	
+	/** creates a data object based on specified object with 
+	 * specified collected fields, and keys fields
+	 * @param fields
+	 * @param keys
+	 * @param model
+	 * @param table
+	 */
+	public DODelegatorEx(String fields, String keys, T model, String table) {
+		super(model);
+		if (fields == null || fields.isEmpty())
+			throw new IllegalArgumentException("At least one data containing field has to be specified");
+		
+		HashMap<String, java.lang.reflect.Field> only = new HashMap<String, java.lang.reflect.Field>();
+		StringTokenizer st = new StringTokenizer(fields, ",");
+		while (st.hasMoreTokens()) {
+			String field = normilizeFieldName( st.nextToken() );
+			if (fieldsMap.containsKey(field)) {
+				only.put(field, fieldsMap.get(field));
+			} else
+				Log.l.debug("The field %s was claimed as data wasn't in a fields list and ignored", field);
+		}
+		if (only.isEmpty())
+			throw new IllegalArgumentException("No one of specified fileds belongs to  data object");
+		if (keys != null) {
+			selectedData = new HashSet<String>();
+			st = new StringTokenizer(keys, ",");
+			while (st.hasMoreTokens()) {
+				String field = normilizeFieldName(st.nextToken());
+				if (fieldsMap.containsKey(field))
+					selectedData.add(field);
+				else
+					Log.l.debug("The field %s was claimed as key wasn't in fields list and ignored", field);
+			}
+		}
+		fieldsMap = only;
 	}
 
 	public DODelegatorEx(T model) {
