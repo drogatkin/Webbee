1e9f8cbeab95bdc7070f8c3f09521b2424d57fc7
dmitriy <jaddressbook@gmail.com>
2022-04-20 17:46:32 -0700

    returnin to the previous approach with a small correction

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,12 +1,12 @@
-var handlerUrl;
+var handlerUrl
 
 function updateUI() {
 	if (!handlerUrl || handlerUrl == '')
-		handlerUrl = './ajax/Asyncupdate';
+		handlerUrl = './ajax/Asyncupdate'
 	
-	var request = new XMLHttpRequest();
-	request.open("GET", handlerUrl, true);
-	request.setRequestHeader("Content-Type", "application/x-javascript;");
+	var request = new XMLHttpRequest()
+	request.open("GET", handlerUrl, true)
+	request.setRequestHeader("Content-Type", "application/x-javascript;")
 	//if (pageMark && pageMark != '')
 	//	request.setRequestHeader("page-mark", pageMark);
 	request.onreadystatechange = function() {
@@ -16,34 +16,34 @@ function updateUI() {
 					//alert(request.responseText);
 					if (request.responseText == '')
 						return;
-					var uievents = null;
+					var uievents = null
 					try {
 						uievents = new Function("return "
-							+ request.responseText)();
+							+ request.responseText)()
 					} catch(e) {
 						// can't parse
 						return;
 					}
 					for ( var i in uievents) {
-						var uie = uievents[i];
+						var uie = uievents[i]
 						//alert(typeof uie.eventHandler + uie.eventHandler);
 						//if (typeof uie.eventHandler == 'function')
 						if (window[uie.eventHandler])
 							try {
-								window[uie.eventHandler].apply(this, uie.parameters || []);
+								window[uie.eventHandler].apply(this, uie.parameters || [])
 							} catch (e) {
 								alert('WebBee: an exception at calling '+uie.eventHandler+' event handler '+e.name+': '+e.message);
 							}
 					}
 				}
-				updateUI();
-			} else if (request.status == 403 || request.status == 404) 
-				return;
-			// old
-		}
-		
-	};
-	request.send(null);
+				
+			} else if (request.status == 403 || request.status == 404 || request.status === 0 ||
+			    request.status >= 500) 
+				return
+			updateUI()
+		}	
+	}
+	request.send(null)
 }
 
 function releaseUI() {
