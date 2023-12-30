b3b57909416f3dd7ece28c8965ee490c69aac212
dmitriy <jaddressbook@gmail.com>
2023-12-30 11:45:21 -0800

    added version info

--- src/java/com/beegman/webbee/base/WebApp.java
+++ src/java/com/beegman/webbee/base/WebApp.java
@@ -15,6 +15,8 @@ import com.beegman.webbee.model.AppModel;
 public class WebApp extends Main {
 	public static final String COMMONDATA_IDENT = "commonlabels";
 	
+	public static final String  VERSION = "1.34";
+	
 	static BaseBehavior commonBehavior; // keeps app specific common behavior
 
 	@Override
@@ -38,7 +40,7 @@ public class WebApp extends Main {
 
 	@Override
 	public String getServletInfo() {
-		return "webbee";
+		return "webbee version " + VERSION;
 	}
 
 	@Override
